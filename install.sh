33339ae8891d89ce56941d0a5263ebe2e411f21e
Evgeny Prokhorov <prohorov-evgen@ya.ru>
2020-04-12 18:42:16 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+txtBlock="\e[33m"
+txtQuestion="\e[36m"
+txtInfo="\e[93m"
+txtSuccess="\e[92m"
+txtError="\e[91m"
+
+txtBold="\e[1m"
+txtEnd="\e[0m"
+
+function printBlock(){
+    symbol="="
+    spaces_count=8
+
+    length=$(echo -n $1 | wc -c)
+    total_lenght=$(($length + 2*$spaces_count + 2))
+
+    line=$(for ((i=1; i<=$total_lenght; i++)); do echo -e -n "$symbol"; done)
+    spaces=$(for ((i=1; i<=$spaces_count; i++)); do echo -e -n " "; done)
+
+    echo -e ""
+    echo -e "$txtBlock$line"
+    echo -e " $spaces$1$spaces "
+    echo -e "$line$txtEnd" 
+    echo -e ""
+}
+
+function printQuestion(){
+    echo -e -n "$txtQuestion$1: $txtEnd"
+}
+
+function printInfo(){
+    echo -e "$txtInfo$txtBold$1$txtEnd"
+}
+
+function printSuccess(){
+    echo -e "$txtSuccess$txtBold$1$txtEnd"
+}
+
+function printError(){
+    echo -e "$txtError$txtBold$1$txtEnd"
+}
+
+printBlock 'PUSHSTAT INSTALLER'
+
+printQuestion 'Enter Prometheus URL (example: http://example.com)'
+read prometheus_url
+if [ -z "$prometheus_url" ]
+then
+    printError "Prometheus URL is required"
+    exit 1
+fi
+
+printQuestion 'Enter Prometheus Pushgateway port (default: 9091)'
+read prometheus_port
+if [ -z "$prometheus_port" ]
+then
+    prometheus_port='9091' 
+fi
+
+printQuestion 'Enter instance name'
+read host_name
+if [ -z "$host_name" ]
+then
+    printError "Instance name is required"
+    exit 1 
+fi
+
+printQuestion 'Enter app folder (default: /var/www)'
+read app_path
+if [ -z "$app_path" ]
+then
+    app_path='/var/www'
+fi
+
+printQuestion 'Enter metrics push interval (default: 15)'
+read interval
+if [ -z "$interval" ]
+then
+    interval='15' 
+fi
+
+mkdir '/etc/pushstat'
+
+echo "prometheus_host=$prometheus_url" > '/etc/pushstat/config.ini'
+echo "prometheus_port=$prometheus_port" >> '/etc/pushstat/config.ini'
+echo "host_name=$host_name" >> '/etc/pushstat/config.ini'
+echo "app_path=$app_path" >> '/etc/pushstat/config.ini'
+echo "interval=$interval" >> '/etc/pushstat/config.ini'
+
+cp pushstat.sh /usr/lib/bin/pushstat
+cp pushstat.service /etc/systemd/system/pushstat.service
+
+
---
+++ pushstat.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+opt_prometeus_host=
+opt_prometeus_port=9091
+opt_host_name=
+opt_app_path='/var/www'
+opt_interval=15
+opt_config=
+opt_test_mode=false
+
+help () {
+  echo
+  usage
+  echo
+  echo "  Simple script for pushing system stats to the Prometheus Pushgateway"
+  echo
+  echo "  Options: "
+  echo "    -n, --name <name>          instance name in prometeus"
+  echo "    -h, --host <address>       prometeus pushgateway host (example: http://example.net)"
+  echo "    -p, --port <port>          prometeus pushgateway port (default: 9091)"  
+  echo "    -a, --app_path <path>      app path (default: /var/www)"     
+  echo "    -i, --interval <seconds>   collect metrics every n seconds (default: 15)"  
+  echo "    -c, --config <path>        set file config"  
+  echo "    -t, --test                 run this script in a test mode"
+  echo "    -h, --help                 show this text"
+  echo
+}
+
+usage () {
+  echo "  Usage: $0 -n -h [-p] [-a] [-i] [-c] [-t]"
+}
+
+push_stat() {
+	if [ $opt_test_mode = true ]; then
+		echo "$1: $2"
+	else
+		echo "$1 $2" | curl --data-binary @- "$opt_prometeus_host:$opt_prometeus_port$/metrics/job/pushgateway/instance/$opt_host_name"		
+	fi
+}
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -n|--name)
+      shift
+      opt_host_name=$1
+      ;;  
+    -h|--host)
+      shift
+      opt_prometeus_host=$1
+      ;;
+    -p|--port)
+      shift
+      opt_prometeus_port=$1
+      ;;   
+    -a|--app_path)
+      shift
+      opt_app_path=$1
+      ;;               	  	
+    -i|--interval)
+      shift
+      opt_interval=$1
+      ;;  	
+    -t|--test)
+      opt_test_mode=true
+      ;;
+    -c|--config)
+	  shift
+      opt_config=$1
+      ;;
+    -h|--help)
+      help
+      exit
+      ;;
+  esac
+
+  shift
+done
+
+if [ -n "$opt_config" ]; then
+	. $opt_config
+
+	opt_prometeus_host=$(awk -F "=" '/prometheus_host/ {print $2}' $opt_config)
+	opt_prometeus_port=$(awk -F "=" '/prometheus_port/ {print $2}' $opt_config)
+	opt_host_name=$(awk -F "=" '/host_name/ {print $2}' $opt_config)
+	opt_interval=$(awk -F "=" '/interval/ {print $2}' $opt_config)
+	opt_app_path=$(awk -F "=" '/app_path/ {print $2}' $opt_config)
+fi
+
+if [ -z "$opt_prometeus_host"] || [ -z "$opt_prometeus_port"] || [ -z "$opt_host_name"] || [ -z "$opt_interval"]
+then
+	echo 'One of required params is empty'
+	exit 1
+fi
+
+while :
+do
+	cpu_usage=$(ps axo %cpu | awk '{ sum+=$1 } END { printf "%.1f\n", sum }' | tail -n 1)
+	push_stat 'cpu_usage' $cpu_usage
+
+	memory_usage=$(free | awk '/Mem:/ {
+	              printf "%.1f", 100 - $4 / ($3 + $4) * 100}')
+	push_stat 'memory_usage' $memory_usage
+
+	swap_usage=$(free |
+	        awk '/Swap/{ if (int($2) == 0) exit; printf "%.1f", $3 / $2 * 100.0 }')
+	push_stat 'swap_usage' $swap_usage
+
+	disk_usage=$(cd $opt_app_path && df . | awk '{if ($1 != "Filesystem") print $5}' | tr -d %)
+	push_stat 'disk_usage' $disk_usage
+
+	ssh_connections=$(who | wc -l)
+	push_stat 'ssh_connections' $ssh_connections
+
+	sleep $opt_interval
+done 
